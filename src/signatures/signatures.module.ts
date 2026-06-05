b8ec80277df1a77eea25424b1d7413f01a5341fc
joselinki25 <josepalof@gmail.com>
2026-06-05 15:12:46 -0500

    Conectamos el validador de identidad en modulo signatures

--- src/signatures/signatures.module.ts
+++ src/signatures/signatures.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { SignaturesController } from './signatures.controller';
 import { SignaturesService } from './signatures.service';
+import { ReniecAdapter } from '../common/adapters/reniec.adapter';
+import { IDENTITY_VALIDATOR_TOKEN } from '../common/interfaces/identity-validator.interface';
 
 @Module({
   controllers: [SignaturesController],
-  providers: [SignaturesService]
+  providers: [
+    SignaturesService,
+    {
+      provide: IDENTITY_VALIDATOR_TOKEN, // Cuando alguien requiers este token
+      useClass: ReniecAdapter, // se entregara una instancia de esta clase
+    },
+  ],
 })
 export class SignaturesModule {}
